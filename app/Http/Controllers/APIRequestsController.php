ee4f5b0beb27f6e01e08fab278d9f7951ca4211d
unknown <miroslav.nedelchev@dentacoin.com>
2019-02-26 13:23:01 +0200

    test contract pdf design

--- app/Http/Controllers/APIRequestsController.php
+++ app/Http/Controllers/APIRequestsController.php
@@ -340,11 +340,13 @@ class APIRequestsController extends Controller {
         $resp = json_decode(curl_exec($curl));
         curl_close($curl);
 
-        if(!empty($resp))   {
+        return $resp;
+
+        /*if(!empty($resp))   {
             return $resp;
         }else {
             return false;
-        }
+        }*/
     }
 
     /*//this method is not from the CoreDB, but from the IPFS NODEJS API on the website server
