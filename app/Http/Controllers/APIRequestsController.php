5e51ac77d137b3836607a200a28835cab177a3ae
unknown <miroslav.nedelchev@dentacoin.com>
2019-02-26 13:52:15 +0200

    test curl

--- app/Http/Controllers/APIRequestsController.php
+++ app/Http/Controllers/APIRequestsController.php
@@ -328,7 +328,7 @@ class APIRequestsController extends Controller {
         curl_setopt_array($curl, array(
             CURLOPT_RETURNTRANSFER => 1,
             CURLOPT_POST => 1,
-            CURLOPT_URL => 'https://dev-test.dentacoin.com/encrypt-data',
+            CURLOPT_URL => 'https://dev-test.dentacoin.com/encrypt',
             CURLOPT_SSL_VERIFYPEER => 0,
             CURLOPT_POSTFIELDS => $json
         ));
