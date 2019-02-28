44a5f8080cd1fccf877bb08ebb8a7af6532f0b05
unknown <miroslav.nedelchev@dentacoin.com>
2019-02-28 20:46:48 +0200

    test profile delete

--- resources/views/pages/logged-user/manage-privacy.blade.php
+++ resources/views/pages/logged-user/manage-privacy.blade.php
@@ -22,7 +22,7 @@
                                 <div class="fs-16 dark-color">We can delete your Profile along with all your personal data from our servers. Just keep in mind that this will terminate your account irreversibly. If you are sure about that, just click the button below.</div>
                             </div>
                             <div class="btn-container text-right padding-top-30">
-                                <form method="POST" action="{{route('delete-my-profile')}}">
+                                <form method="POST" action="{{route('delete-my-profile')}}" onsubmit="return confirm('Are you sure you want to continue?')">
                                     <input type="submit" value="DELETE MY PROFILE & PERSONAL DATA" class="white-blue-green-btn"/>
                                 </form>
                             </div>
