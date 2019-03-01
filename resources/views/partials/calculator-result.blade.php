95a53cebff716eee1929b71d6d1fd9c3d8441442
unknown <miroslav.nedelchev@dentacoin.com>
2019-03-01 10:29:31 +0200

    fix fixed calulcator btn and calculator popup to call login popup

--- resources/views/partials/calculator-result.blade.php
+++ resources/views/partials/calculator-result.blade.php
@@ -5,9 +5,15 @@
     </div>
 </div>
 <div class="text-center fs-16 padding-top-50 padding-bottom-50 width-70 margin-0-auto">* The result is calculated based on the suggested average Assurance premiums for your country. Premiums may vary depending on your personal agreement with your patients.</div>
-<div class="text-center">
-    <a href="javascript:void(0)" class="white-blue-green-btn min-width-250 show-login-signin">ENROLL DENTACOIN ASSURANCE</a>
-</div>
-<div class="text-center padding-top-30 padding-bottom-40">
-    <a href="javascript:void(0)" class="fs-20 lato-regular blue-green-color calculate-again">or calculate again</a>
-</div>
+@if((new \App\Http\Controllers\UserController())->checkSession())
+    <div class="text-center padding-top-30 padding-bottom-40">
+        <a href="javascript:void(0)" class="fs-20 lato-regular blue-green-color calculate-again">calculate again</a>
+    </div>
+@else
+    <div class="text-center">
+        <a href="javascript:void(0)" class="white-blue-green-btn min-width-250 show-login-signin">ENROLL DENTACOIN ASSURANCE</a>
+    </div>
+    <div class="text-center padding-top-30 padding-bottom-40">
+        <a href="javascript:void(0)" class="fs-20 lato-regular blue-green-color calculate-again">or calculate again</a>
+    </div>
+@endif
