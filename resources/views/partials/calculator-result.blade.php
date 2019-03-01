5af1b92655b71beda761133aeec9bc68d1623c62
unknown <miroslav.nedelchev@dentacoin.com>
2019-03-01 10:34:46 +0200

    fix fixed calulcator btn and calculator popup to call login popup

--- resources/views/partials/calculator-result.blade.php
+++ resources/views/partials/calculator-result.blade.php
@@ -6,7 +6,7 @@
 </div>
 <div class="text-center fs-16 padding-top-50 padding-bottom-50 width-70 margin-0-auto">* The result is calculated based on the suggested average Assurance premiums for your country. Premiums may vary depending on your personal agreement with your patients.</div>
 @if((new \App\Http\Controllers\UserController())->checkSession())
-    <div class="text-center padding-top-30 padding-bottom-40">
+    <div class="text-center padding-bottom-40">
         <a href="javascript:void(0)" class="fs-20 lato-regular blue-green-color calculate-again">calculate again</a>
     </div>
 @else
