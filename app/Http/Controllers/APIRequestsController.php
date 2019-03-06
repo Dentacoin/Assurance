b06c7b3289f8e84a696acd9d1f99cd1eb3bc0cfb
unknown <miroslav.nedelchev@dentacoin.com>
2019-03-06 19:00:06 +0200

    test withdraw

--- app/Http/Controllers/APIRequestsController.php
+++ app/Http/Controllers/APIRequestsController.php
@@ -345,7 +345,7 @@ class APIRequestsController extends Controller {
     public function encryptFile($key, $html) {
         $curl = curl_init();
         $json = '{"public_key":"'.$key.'", "html":"'.$html.'"}';
-        var_dump($json);
+        var_dump(json_decode($json));
         die();
         curl_setopt_array($curl, array(
             CURLOPT_RETURNTRANSFER => 1,
