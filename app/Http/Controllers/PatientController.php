bc509adf0d2fc7d88ba4c0d1665f804f70e62b40
unknown <miroslav.nedelchev@dentacoin.com>
2019-03-06 18:57:33 +0200

    test withdraw

--- app/Http/Controllers/PatientController.php
+++ app/Http/Controllers/PatientController.php
@@ -313,10 +313,14 @@ class PatientController extends Controller {
 
         $view_end = view('partials/pdf-contract-layout-end');
         $html_end = $view_end->render();
+        var_dump(htmlentities($html_body));
+        die();
+
+        var_dump(htmlentities($this->minifyHtmlParts($html_body)));
 
         //sending the pdf html to encryption nodejs api
-        $encrypted_html_by_patient = (new \App\Http\Controllers\APIRequestsController())->encryptFile($patient_pub_key->public_key, htmlentities($this->minifyHtmlParts($html_body)));
-        $encrypted_html_by_dentist = (new \App\Http\Controllers\APIRequestsController())->encryptFile($dentist_pub_key->public_key, htmlentities($this->minifyHtmlParts($html_body)));
+        $encrypted_html_by_patient = (new \App\Http\Controllers\APIRequestsController())->encryptFile($patient_pub_key->public_key, htmlentities($html_body));
+        $encrypted_html_by_dentist = (new \App\Http\Controllers\APIRequestsController())->encryptFile($dentist_pub_key->public_key, htmlentities($html_body));
 
         var_dump($encrypted_html_by_patient);
         var_dump($encrypted_html_by_dentist);
