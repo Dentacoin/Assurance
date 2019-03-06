285d54b0ba59f77497cd776b78af17257927ff00
unknown <miroslav.nedelchev@dentacoin.com>
2019-03-06 19:40:37 +0200

    test withdraw

--- app/Http/Controllers/APIRequestsController.php
+++ app/Http/Controllers/APIRequestsController.php
@@ -344,10 +344,8 @@ class APIRequestsController extends Controller {
     //this method is not from the CoreDB, but from the IPFS NODEJS API on the website server
     public function encryptFile($key, $html) {
         $curl = curl_init();
-        $json = '{"public_key":"'.$key.'", "html":"'.str_replace('&quot;',"&apos;" , htmlentities('<label class="inline-block">Professional / Company Registration Number:</label><label class="inline-block">Professional / Company Registration Number:</label><label class="inline-block">Professional / Company Registration Number:</label><label class="inline-block">Professional / Company Registration Number:</label><label class="inline-block">Professional / Company Registration Number:</label><label class="inline-block">Professional / Company Registration Number:</label><label class="inline-block">Professional / Company Registration Number:</label><label class="inline-block">Professional / Company Registration Number:</label><label class="inline-block">Professional / Company Registration Number:</label><label class="inline-block">Professional / Company Registration Number:</label><label class="inline-block">Professional / Company Registration Number:</label><label class="inline-block">Professional / Company Registration Number:</label><label class="inline-block">Professional / Company Registration Number:</label><label class="inline-block">Professional / Company Registration Number:</label><label class="inline-block">Professional / Company Registration Number:</label><label class="inline-block">Professional / Company Registration Number:</label><label class="inline-block">Professional / Company Registration Number:</label><label class="inline-block">Professional / Company Registration Number:</label><label class="inline-block">Professional / Company Registration Number:</label><label class="inline-block">Professional / Company Registration Number:</label><label class="inline-block">Professional / Company Registration Number:</label><label class="inline-block">Professional / Company Registration Number:</label><label class="inline-block">Professional / Company Registration Number:</label><label class="inline-block">Professional / Company Registration Number:</label><label class="inline-block">Professional / Company Registration Number:</label>')).'"}';
+        $json = '{"public_key":"'.$key.'", "html":"'.str_replace('&quot;',"&apos;" , $html).'"}';
 
-        var_dump(json_decode($json));
-        die();
         curl_setopt_array($curl, array(
             CURLOPT_RETURNTRANSFER => 1,
             CURLOPT_POST => 1,
@@ -362,8 +360,6 @@ class APIRequestsController extends Controller {
 
         $resp = json_decode(curl_exec($curl));
         curl_close($curl);
-
-        var_dump($resp);
 
         if(!empty($resp))   {
             return $resp;
