e47e872ed6e4a132acd2c8e59fb6e6d05c07c603
unknown <miroslav.nedelchev@dentacoin.com>
2019-03-06 16:33:43 +0200

    test transaction

--- app/Http/Controllers/APIRequestsController.php
+++ app/Http/Controllers/APIRequestsController.php
@@ -374,8 +374,6 @@ class APIRequestsController extends Controller {
         $header[] = 'Authorization: Bearer ' . session('logged_user')['token'];
         $header[] = 'Cache-Control: no-cache';
 
-        var_dump($data);
-
         $curl = curl_init();
         curl_setopt_array($curl, array(
             CURLOPT_RETURNTRANSFER => 1,
