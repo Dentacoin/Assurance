94e0b590cf5382884e4cd05681b1033c5c5626ad
unknown <miroslav.nedelchev@dentacoin.com>
2019-03-07 07:56:11 +0200

    test pdf generation

--- app/Http/Controllers/APIRequestsController.php
+++ app/Http/Controllers/APIRequestsController.php
@@ -345,6 +345,9 @@ class APIRequestsController extends Controller {
     public function encryptFile($key, $html) {
         $curl = curl_init();
         $json = '{"public_key":"'.$key.'", "html":"'.str_replace('"', "'", $html).'"}';
+        var_dump($json);
+        var_dump(json_decode($json));
+        die();
 
         curl_setopt_array($curl, array(
             CURLOPT_RETURNTRANSFER => 1,
