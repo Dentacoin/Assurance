2ebef80f0d01fa1ce5383bffd150a72a9d6bceb4
unknown <miroslav.nedelchev@dentacoin.com>
2019-03-07 23:07:02 +0200

    test ipfs uploading

--- app/Http/Controllers/APIRequestsController.php
+++ app/Http/Controllers/APIRequestsController.php
@@ -332,7 +332,7 @@ class APIRequestsController extends Controller {
             'Content-Length: ' . mb_strlen($json))
         );
 
-        $resp = curl_exec($curl);
+        $resp = json_decode(curl_exec($curl));
         curl_close($curl);
 
         if(!empty($resp))   {
