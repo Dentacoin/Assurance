66beda32f49adc07f89ae8000a391d9e5a611265
unknown <miroslav.nedelchev@dentacoin.com>
2019-03-08 18:01:57 +0200

    test contact with clinic

--- resources/views/partials/contract-terms-and-conditions.blade.php
+++ resources/views/partials/contract-terms-and-conditions.blade.php
@@ -3,44 +3,44 @@
     <div class="padding-bottom-20">This present Dentacoin Assurance Contract Agreement was reached on ------ <span class="calibri-bold">BETWEEN</span> <span class="calibri-bold">and</span></div>
 @endif
 <h3 class="fs-22 calibri-bold padding-top-30 padding-bottom-15">1. DENTIST RIGHTS AND OBLIGATIONS</h3>
-<div class="padding-bottom-10">This present Dentacoin Assurance Contract obliges the Dentist to provide free of charge to the Patient [number of check-ups] check-ups per year and [number of tooth cleanings] professional tooth cleanings per year for the duration of the contract.</div>
-<div class="padding-bottom-10">The Dentist is also obliged to cover the costs for their work for the predefined set of possibly occurring treatments described in 3. Service Coverage.</div>
-<div class="padding-bottom-10">The Dentist has the right to withdraw the agreed monthly premium amount in Dentacoin (DCN) currency on the payment due date or later once the Patient has given their initial permission.</div>
-<div class="padding-bottom-10">The Dentist has the right to cancel the contract and/ or refuse a treatment in case the Patient has not visit the Dentist for the required number of check-ups and tooth cleanings per year. </div>
+<div class="padding-bottom-15">This present Dentacoin Assurance Contract obliges the Dentist to provide free of charge to the Patient [number of check-ups] check-ups per year and [number of tooth cleanings] professional tooth cleanings per year for the duration of the contract.</div>
+<div class="padding-bottom-15">The Dentist is also obliged to cover the costs for their work for the predefined set of possibly occurring treatments described in 3. Service Coverage.</div>
+<div class="padding-bottom-15">The Dentist has the right to withdraw the agreed monthly premium amount in Dentacoin (DCN) currency on the payment due date or later once the Patient has given their initial permission.</div>
+<div class="padding-bottom-15">The Dentist has the right to cancel the contract and/ or refuse a treatment in case the Patient has not visit the Dentist for the required number of check-ups and tooth cleanings per year. </div>
 <h3 class="fs-22 calibri-bold padding-top-30 padding-bottom-15">2. PATIENT RIGHTS AND OBLIGATIONS</h3>
-<div class="padding-bottom-10">This present Dentacoin Assurance Contract obliges the Patient to charge their wallet every month with the agreed assurance premium amount in Dentacoin (DCN) currency, namely an equivalent to [monthly assurance fee in USD] USD. The Patient must ensure that they have the full amount needed in their wallet latest on the payment due date ([payment due date]).</div>
-<div class="padding-bottom-10">The Patient is also obliged to visit the Dentist for [number of check-ups] check-ups per year and [number of tooth cleanings] professional tooth cleanings per year, as well as to use Dentacare Mobile app consistently for at least 90 days.</div>
-<div class="padding-bottom-10">In case the above-described recommendations are followed, the Patient has the right to have their occurring treatment costs covered for the predefined set of possibly occurring treatments described in 3. Service Coverage.</div>
-<div class="padding-bottom-10">The Patient has the right to cancel the contract at any given moment.</div>
+<div class="padding-bottom-15">This present Dentacoin Assurance Contract obliges the Patient to charge their wallet every month with the agreed assurance premium amount in Dentacoin (DCN) currency, namely an equivalent to [monthly assurance fee in USD] USD. The Patient must ensure that they have the full amount needed in their wallet latest on the payment due date ([payment due date]).</div>
+<div class="padding-bottom-15">The Patient is also obliged to visit the Dentist for [number of check-ups] check-ups per year and [number of tooth cleanings] professional tooth cleanings per year, as well as to use Dentacare Mobile app consistently for at least 90 days.</div>
+<div class="padding-bottom-15">In case the above-described recommendations are followed, the Patient has the right to have their occurring treatment costs covered for the predefined set of possibly occurring treatments described in 3. Service Coverage.</div>
+<div class="padding-bottom-15">The Patient has the right to cancel the contract at any given moment.</div>
 <h3 class="fs-22 calibri-bold padding-top-30 padding-bottom-15">3. DENTAL SERVICES COVERAGE</h3>
 <div class="fs-18 calibri-bold">Prophylaxis visits:</div>
 <ul>
     <li>[number of check-ups] check-ups per year;</li>
     <li>[number of tooth cleanings] professional tooth cleanings per year.</li>
 </ul>
-<div class="fs-18 calibri-bold">General Dentistry:</div>
+<div class="fs-18 calibri-bold padding-top-15">General Dentistry:</div>
 <ul>
     <li>Fillings;</li>
     <li>Caries infiltration;</li>
     <li>Tooth extraction.</li>
 </ul>
 <h3 class="fs-22 calibri-bold padding-top-30 padding-bottom-15">4. PAYMENTS</h3>
-<div class="padding-bottom-10">The agreed monthly assurance premium between the Dentist and the Patient is equivalent to [monthly premium] USD in Dentacoin (DCN) currency and must be paid every month on the agreed payment due date ([payment due date]).</div>
-<div class="padding-bottom-10">The Patient must ensure that they have the full amount needed in their wallet on the payment due date. There is a grace period of 21 (twenty one) days.</div>
-<div class="padding-bottom-10">The Dentist has the right to withdraw the agreed monthly premium amount in Dentacoin (DCN) currency on the payment due date or later once the Patient has given their initial permission.</div>
-<div class="padding-bottom-10">Both parties must only used the wallets explicitly specified in this contract.</div>
+<div class="padding-bottom-15">The agreed monthly assurance premium between the Dentist and the Patient is equivalent to [monthly premium] USD in Dentacoin (DCN) currency and must be paid every month on the agreed payment due date ([payment due date]).</div>
+<div class="padding-bottom-15">The Patient must ensure that they have the full amount needed in their wallet on the payment due date. There is a grace period of 21 (twenty one) days.</div>
+<div class="padding-bottom-15">The Dentist has the right to withdraw the agreed monthly premium amount in Dentacoin (DCN) currency on the payment due date or later once the Patient has given their initial permission.</div>
+<div class="padding-bottom-15">Both parties must only used the wallets explicitly specified in this contract.</div>
 <h3 class="fs-22 calibri-bold padding-top-30 padding-bottom-15">5. COMPATIBILITY WITH OTHER DENTAL PLANS</h3>
-<div class="padding-bottom-10">Dentacoin Assurance does not compete with other public and private health insurance, assurance or membership plans. The Dentist is free to decide which plans to work with. They can either offer Dentacoin Assurance as a complementary plan which will cover/ reduce the Patient’s out-of-pocket costs or as a stand-alone plan in case other options are not available.</div>
+<div class="padding-bottom-15">Dentacoin Assurance does not compete with other public and private health insurance, assurance or membership plans. The Dentist is free to decide which plans to work with. They can either offer Dentacoin Assurance as a complementary plan which will cover/ reduce the Patient’s out-of-pocket costs or as a stand-alone plan in case other options are not available.</div>
 <h3 class="fs-22 calibri-bold padding-top-30 padding-bottom-15">6. CANCELLATION POLICY</h3>
-<div class="padding-bottom-10">The Dentist has the right to cancel the contract and/ or refuse a treatment in case the Patient has not visit the Dentist for the required number of check-ups and tooth cleanings per year.</div>
-<div class="padding-bottom-10">The Patient has the right to cancel the contract at any given moment.</div>
-<div class="padding-bottom-10">In case the Patient has not paid the agreed monthly premium, the contract is automatically cancelled.</div>
+<div class="padding-bottom-15">The Dentist has the right to cancel the contract and/ or refuse a treatment in case the Patient has not visit the Dentist for the required number of check-ups and tooth cleanings per year.</div>
+<div class="padding-bottom-15">The Patient has the right to cancel the contract at any given moment.</div>
+<div class="padding-bottom-15">In case the Patient has not paid the agreed monthly premium, the contract is automatically cancelled.</div>
 <h3 class="fs-22 calibri-bold padding-top-30 padding-bottom-15">7. COMPLAINTS AND THIRD-PARTY REGULATIONS</h3>
-<div class="padding-bottom-10">This present Agreement is concluded directly between the Patient and the Dentist.</div>
-<div class="padding-bottom-10">Dentacoin B.V. only provides the infrastructure to help both parties communicate and exchange value easier. If any technical difficulties are faced when using the website, Dentacoin B.V. can be contacted at <a href="mailto:assurance@dentacoin.com">assurance@dentacoin.com</a>.</div>
-<div class="padding-bottom-10">It is not Dentacoin B.V.’s responsibility to handle any complaints in regards to the violation of this contract.</div>
+<div class="padding-bottom-15">This present Agreement is concluded directly between the Patient and the Dentist.</div>
+<div class="padding-bottom-15">Dentacoin B.V. only provides the infrastructure to help both parties communicate and exchange value easier. If any technical difficulties are faced when using the website, Dentacoin B.V. can be contacted at <a href="mailto:assurance@dentacoin.com">assurance@dentacoin.com</a>.</div>
+<div class="padding-bottom-15">It is not Dentacoin B.V.’s responsibility to handle any complaints in regards to the violation of this contract.</div>
 <h3 class="fs-22 calibri-bold padding-top-30 padding-bottom-15">8. DATA PRIVACY</h3>
-<div class="padding-bottom-10">Dentacoin B.V. takes the protection of personal data very seriously. Personal data are treated as confidential and in accordance with the statutory data protection regulations and this Privacy Policy: <a href="//dentacoin.com/privacy-policy" target="_blank">https://dentacoin.com/privacy-policy</a>.</div>
+<div class="padding-bottom-15">Dentacoin B.V. takes the protection of personal data very seriously. Personal data are treated as confidential and in accordance with the statutory data protection regulations and this Privacy Policy: <a href="//dentacoin.com/privacy-policy" target="_blank">https://dentacoin.com/privacy-policy</a>.</div>
 @if(!empty($contract->contract_active_at))
     {{var_dump('SHOW PATIENT AND DENTIST SIGNATURES')}}
 @endif
