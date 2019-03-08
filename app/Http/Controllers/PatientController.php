7268917be3dc4cc06a0dc259f8b6b7acc8ab9e82
unknown <miroslav.nedelchev@dentacoin.com>
2019-03-08 16:13:01 +0200

    test contact with clinic

--- app/Http/Controllers/PatientController.php
+++ app/Http/Controllers/PatientController.php
@@ -453,7 +453,7 @@ class PatientController extends Controller {
             $title = 'Dr. ';
         }
 
-        $body = '<div class="padding-bottom-25 padding-top-15">Dear '.$title.'<span class="calibri-bold">'.$clinic->name.'</span> I\'d like to receive a Dentacoin Assurance contract sample by you.<br><br><br><a href="'.BASE_URL.'dentist/create-contract" style="font-size: 20px;color: #126585;background-color: white;padding: 10px 20px;text-decoration: none;font-weight: bold;border-radius: 4px;border: 2px solid #126585;" target="_blank">CREATE CONTRACT SAMPLE</a><br><br><br>';
+        $body = '<div class="padding-bottom-25 padding-top-15">Dear '.$title.'<span class="calibri-bold">'.$clinic->name.'</span>, <br> I\'d like to receive a Dentacoin Assurance contract sample by you.<br><br><a href="'.BASE_URL.'dentist/create-contract" style="font-size: 20px;color: #126585;background-color: white;padding: 10px 20px;text-decoration: none;font-weight: bold;border-radius: 4px;border: 2px solid #126585;" target="_blank">CREATE CONTRACT SAMPLE</a><br><br>';
 
         $view = view('partials/before-sending-email-confirmation-popup', ['sender' => $current_patient, 'receiver' => $receiver, 'mail_title' => 'Send email to your dentist', 'mail_subject' => 'Please send me a Dentacoin Assurance contract sample', 'mail_body' => $body]);
         $view = $view->render();
@@ -479,7 +479,8 @@ class PatientController extends Controller {
         $body = '<!DOCTYPE html><html><head></head><body><div style="font-size: 16px;">Dear '.$title.$clinic->name.', <br><br><br>I\'d like to receive a Dentacoin Assurance contract sample by you.<br><br><br><a href="'.BASE_URL.'dentist/create-contract" style="font-size: 20px;color: #126585;background-color: white;padding: 10px 20px;text-decoration: none;font-weight: bold;border-radius: 4px;border: 2px solid #126585;" target="_blank">CREATE CONTRACT SAMPLE</a><br><br><br><i style="font-size: 13px;">* Blockchain is just a new technology used for secure storage and exchange of value and data.</i></div></body></html>';
 
         Mail::send(array(), array(), function($message) use ($body, $clinic, $sender) {
-            $message->to($clinic->email)->subject('Please send me a Dentacoin Assurance contract sample');
+            //$message->to($clinic->email)->subject('Please send me a Dentacoin Assurance contract sample');
+            $message->to('testyserver@abv.bg')->subject('Please send me a Dentacoin Assurance contract sample');
             $message->from($sender->email, $sender->name)->replyTo($sender->email, $sender->name);
             $message->setBody($body, 'text/html');
         });
