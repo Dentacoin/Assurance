c4df8327c3592ae6027fe42332227973c0c4c124
unknown <miroslav.nedelchev@dentacoin.com>
2019-03-08 15:02:09 +0200

    unify email confirmation template

---
+++ resources/views/partials/before-sending-email-confirmation-popup.blade.php
@@ -0,0 +1,12 @@
+<div class="dark-color fs-18">
+    <div class="text-center fs-40 lato-regular padding-bottom-35"><i class="fa fa-envelope blue-green-color" aria-hidden="true"></i> {{$title}}</div>
+    <div class="single-line padding-top-10 padding-bottom-5 calibri-bold"><label>Receiver:</label> {{$receiver}}</div>
+    <div class="single-line padding-top-10 padding-bottom-5"><label class="calibri-bold">Subject:</label> {{$subject}}</div>
+    <div class="email-body">{{$body}}</div>
+    <div class="padding-bottom-40">
+        Regards,
+        <div class="calibri-bold">{{$sender->name}}</div>
+    </div>
+    <div class="fs-14 calibri-light padding-bottom-30">* Blockchain is just a new technology used for secure storage and exchange of value and data. </div>
+    <div class="text-center padding-bottom-15 send-mail-invite-dentists"><a href="javascript:void(0)" class="white-blue-green-btn min-width-250">SEND</a></div>
+</div>
