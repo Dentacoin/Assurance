0423c76949f796ee3a62fb0879f6082af3dbe2a9
unknown <miroslav.nedelchev@dentacoin.com>
2019-03-12 20:38:07 +0200

    test captcha

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -2287,13 +2287,13 @@ function bindLoginSigninPopupShow() {
                                     }
 
                                     //check captcha
-                                    var check_captcha_response = await checkCaptcha($('.dentist .form-register .step.third #register-captcha').val().trim());
+                                    /*var check_captcha_response = await checkCaptcha($('.dentist .form-register .step.third #register-captcha').val().trim());
                                     if(!$('.dentist .form-register .step.third #register-captcha').length) {
                                         window.location.reload();
                                     } else if(check_captcha_response.error || $('.dentist .form-register .step.third #register-captcha').val().trim() == '') {
                                         customErrorHandle($('.step.third .step-errors-holder'), 'Please enter correct captcha.');
                                         errors = true;
-                                    }
+                                    }*/
 
                                     if(!errors) {
                                         //submit the form
