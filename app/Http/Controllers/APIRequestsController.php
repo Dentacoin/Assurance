2ec9f03563efb2528d7e400117cd1b00705584b2
unknown <miroslav.nedelchev@dentacoin.com>
2019-03-13 17:47:10 +0200

    push response profile pages and test gdpr download

--- app/Http/Controllers/APIRequestsController.php
+++ app/Http/Controllers/APIRequestsController.php
@@ -496,7 +496,7 @@ class APIRequestsController extends Controller {
         curl_setopt_array($curl, array(
             CURLOPT_RETURNTRANSFER => 1,
             CURLOPT_POST => 1,
-            CURLOPT_URL => ' https://api.dentacoin.com/api/gdpr/',
+            CURLOPT_URL => 'https://api.dentacoin.com/api/gdpr/',
             CURLOPT_SSL_VERIFYPEER => 0,
             CURLOPT_HTTPHEADER => $header
         ));
