7d821cd0abc766cfbfe4d9e71a78e148776df3ec
unknown <miroslav.nedelchev@dentacoin.com>
2019-03-23 07:33:07 +0200

    check url

--- bootstrap.php
+++ bootstrap.php
@@ -1,4 +1,10 @@
 <?php
+$actual_link = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+
+var_dump($actual_link);
+die();
+
+
 //define PATHS
 define('DS', '/');
 define('ROOT', realpath(dirname(__FILE__)) . DS);
