9ca489642baf698eec2900a9ec85fc1146243722
unknown <miroslav.nedelchev@dentacoin.com>
2019-04-01 12:23:37 +0300

    fix transaction history

--- resources/views/pages/logged-user/my-profile.blade.php
+++ resources/views/pages/logged-user/my-profile.blade.php
@@ -78,7 +78,7 @@
                                     </form>
                                     @if(!empty($transaction_history))
                                         <h3 class="line-crossed-title margin-bottom-40 fs-20 margin-top-50 lato-bold black-color"><span>Withdraw History</span></h3>
-                                        <table class="transaction-history-table table table-without-reorder table-striped text-left">
+                                        <table>
                                             <thead>
                                                 <tr>
                                                     <th>Date</th>
