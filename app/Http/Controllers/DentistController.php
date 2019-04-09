d56b5326509646c8c594a293d406c3eabf460124
unknown <miroslav.nedelchev@dentacoin.com>
2019-04-09 14:02:49 +0300

    test contract cancellation

--- app/Http/Controllers/DentistController.php
+++ app/Http/Controllers/DentistController.php
@@ -23,6 +23,7 @@ class DentistController extends Controller
             if($contract->status == 'active') {
                 $check_if_legit_contract = (new APIRequestsController())->cancelIfLatePayment($contract->patient_address, $contract->dentist_address);
                 var_dump($check_if_legit_contract);
+                var_dump($check_if_legit_contract->error);
                 die();
                 if($check_if_legit_contract && $check_if_legit_contract->success) {
                     //IF NORMAL PERIOD AND GRACE PERIOD PASSED CANCEL THIS CONTRACT
