d6dfd93031518d38af0e5effd5d4f116a3b0bd2d
unknown <miroslav.nedelchev@dentacoin.com>
2019-04-11 08:33:07 +0300

    change the way of calling login popup

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -346,7 +346,7 @@
                             <div class="padding-bottom-10">
                                 <select name="country-code" id="dentist-country" class="custom-input country-select">
                                     @php($current_phone_code = '+')
-                                    @php($current_user_country_code = mb_strtolower(trim(file_get_contents('http://ipinfo.io/' . $_SERVER['REMOTE_ADDR'] .  '/country'))))
+                                    @php($current_user_country_code = mb_strtolower(trim(file_get_contents('http://ipinfo.io/' . (new Controller())->getClientIp() . '/country'))))
                                     @php($countries = (new \App\Http\Controllers\APIRequestsController())->getAllCountries())
                                     @if(!empty($countries))
                                         @foreach($countries as $country)
