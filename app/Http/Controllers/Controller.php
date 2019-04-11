3a1005d5335d9eeb333900eb923a5b65847a36b8
unknown <miroslav.nedelchev@dentacoin.com>
2019-04-11 08:31:20 +0300

    change the way of calling login popup

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -194,6 +194,25 @@ class Controller extends BaseController
         }
     }
 
+    function getClientIp() {
+        $ipaddress = '';
+        if (getenv('HTTP_CLIENT_IP'))
+            $ipaddress = getenv('HTTP_CLIENT_IP');
+        else if(getenv('HTTP_X_FORWARDED_FOR'))
+            $ipaddress = getenv('HTTP_X_FORWARDED_FOR');
+        else if(getenv('HTTP_X_FORWARDED'))
+            $ipaddress = getenv('HTTP_X_FORWARDED');
+        else if(getenv('HTTP_FORWARDED_FOR'))
+            $ipaddress = getenv('HTTP_FORWARDED_FOR');
+        else if(getenv('HTTP_FORWARDED'))
+            $ipaddress = getenv('HTTP_FORWARDED');
+        else if(getenv('REMOTE_ADDR'))
+            $ipaddress = getenv('REMOTE_ADDR');
+        else
+            $ipaddress = 'UNKNOWN';
+        return $ipaddress;
+    }
+
     /*protected function getApiCountries() {
         if((new UserController())->checkSession()) {
             return (new APIRequestsController())->getAllCountries();
