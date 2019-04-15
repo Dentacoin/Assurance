b4232d3f819e37cdf2666e9fe68ecc63f58b33f7
unknown <miroslav.nedelchev@dentacoin.com>
2019-04-15 10:15:32 +0300

    remove external accounts requests

--- public/assets/js/index-bundled.js
+++ public/assets/js/index-bundled.js
@@ -58104,7 +58104,7 @@ function bindVerifyAddressEvent(keystore_file, render_pdf, encrypted_pdf_content
     $('.proof-of-address .verify-address-btn').click(function() {
         if(keystore_file != null) {
             //import with keystore
-            if(JSON.parse(keystore_file).address.toLowerCase() != $('.proof-of-address').attr('data-address').toLowerCase()) {
+            if(JSON.parse(keystore_file).address.toLowerCase() != '0x' + $('.proof-of-address').attr('data-address').toLowerCase()) {
                 basic.showAlert('Please enter valid keystore file for your Wallet Address.', '', true);
             } else if($('.proof-of-address #your-secret-key-password').val().trim() == '' || $('.proof-of-address #your-secret-key-password').val().trim().length > 100 || $('.proof-of-address #your-secret-key-password').val().trim().length < 6) {
                 basic.showAlert('Please enter valid secret key password with length between 6 and 100 symbols.', '', true);
@@ -58305,7 +58305,7 @@ function bindCacheKeyEvent(keystore_file) {
     $('.proof-of-address .cache-key-btn').click(function() {
         if(keystore_file != null) {
             //import with keystore
-            if(JSON.parse(keystore_file).address.toLowerCase() != $('.proof-of-address').attr('data-address').toLowerCase()) {
+            if(JSON.parse(keystore_file).address.toLowerCase() != '0x' + $('.proof-of-address').attr('data-address').toLowerCase()) {
                 basic.showAlert('Please enter valid keystore file for your Wallet Address.', '', true);
             } else if($('.proof-of-address #your-secret-key-password').val().trim() == '' || $('.proof-of-address #your-secret-key-password').val().trim().length > 100 || $('.proof-of-address #your-secret-key-password').val().trim().length < 6) {
                 basic.showAlert('Please enter valid secret key password with length between 6 and 100 symbols.', '', true);
