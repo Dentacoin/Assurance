6f14e4373d576fcb91ed807190ab754aafd421db
unknown <miroslav.nedelchev@dentacoin.com>
2019-05-17 17:07:03 +0300

    test contract creation

--- app/Http/Controllers/APIRequestsController.php
+++ app/Http/Controllers/APIRequestsController.php
@@ -394,8 +394,7 @@ class APIRequestsController extends Controller {
             'Content-Length: ' . mb_strlen($json))
         );
 
-        $resp = curl_exec($curl);
-        //$resp = json_decode(curl_exec($curl));
+        $resp = json_decode(curl_exec($curl));
         curl_close($curl);
 
         return $resp;
