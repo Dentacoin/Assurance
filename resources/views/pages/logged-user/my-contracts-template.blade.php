29b3054a4c9c061421ee04bc6a486f011a6b4d67
unknown <miroslav.nedelchev@dentacoin.com>
2019-06-19 08:22:55 +0300

    fix my contracts style

--- resources/views/pages/logged-user/my-contracts-template.blade.php
+++ resources/views/pages/logged-user/my-contracts-template.blade.php
@@ -1,5 +1,18 @@
-@extends("layout")
-@section("content")
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
+    <link rel="shortcut icon" href="{{URL::asset('assets/images/favicon.png') }}" type="image/x-icon" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=5" />
+    <meta name="csrf-token" content="{{ csrf_token() }}"/>
+    <link rel="stylesheet" type="text/css" href="/dist/css/front-libs-style.css?v=1.0.1">
+    <link rel="stylesheet" type="text/css" href="/assets/css/style.css?v=1.0.1">
+    <script>
+        var HOME_URL = '{{ route("home") }}';
+    </script>
+</head>
+<body class="@if(\App\Http\Controllers\UserController::instance()->checkSession()) logged-in @endif">
     <div class="my-profile-page-content my-contracts-container inline-block-top">
         <div class="profile-page-title">
             <figure itemscope="" itemtype="http://schema.org/ImageObject" class="inline-block">
@@ -112,4 +125,12 @@
             </figure>
         @endif
     </div>
-@endsection
+    {{--/Show the sticky calculate button only for dentists--}}
+    <script src="/assets/js/basic.js?v=1.0.1"></script>
+    <script src="/dist/js/front-libs-script.js?v=1.0.1"></script>
+    {{--<script src="/dist/js/front-script.js?v=1.0.13"></script>--}}
+    <script src="/assets/js/address.js?v=1.0.1"></script>
+    <script src="/assets/js/index-bundled.js?v=1.0.1"></script>
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCaVeHq_LOhQndssbmw-aDnlMwUG73yCdk&libraries=places&language=en"></script>
+</body>
+</html>
