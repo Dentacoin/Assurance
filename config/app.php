015a0d8bd87c9912e1cf94ff9c864f4b93713151
unknown <miroslav.nedelchev@dentacoin.com>
2019-09-25 08:23:18 +0300

    added cors settings

--- config/app.php
+++ config/app.php
@@ -180,8 +180,7 @@ return [
         App\Providers\AgentServiceProvider::class,
         Roumen\Sitemap\SitemapServiceProvider::class,
         Chumper\Zipper\ZipperServiceProvider::class,
-        Mews\Captcha\CaptchaServiceProvider::class,
-        Barryvdh\Cors\ServiceProvider::class,
+        Mews\Captcha\CaptchaServiceProvider::class
     ],
 
     /*
