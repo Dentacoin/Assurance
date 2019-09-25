8156bf100929058ef8e9b80ce8755e1ad7ccf9f0
unknown <miroslav.nedelchev@dentacoin.com>
2019-09-25 08:35:14 +0300

    added cors settings

--- config/cors.php
+++ config/cors.php
@@ -13,7 +13,7 @@ return [
     */
    
     'supportsCredentials' => false,
-    'allowedOrigins' => ['*'],
+    'allowedOrigins' => ['https://assurance.dentacoin.com/save-public-key'],
     'allowedOriginsPatterns' => [],
     'allowedHeaders' => ['*'],
     'allowedMethods' => ['*'],
