562f025ebcd30db529698a0654984525f3f435e3
unknown <miroslav.nedelchev@dentacoin.com>
2019-10-02 08:35:30 +0300

    update cors

--- config/cors.php
+++ config/cors.php
@@ -13,7 +13,7 @@ return [
     */
    
     'supportsCredentials' => false,
-    'allowedOrigins' => ['https://wallet.dentacoin.com'],
+    'allowedOrigins' => ['*'],
     'allowedOriginsPatterns' => [],
     'allowedHeaders' => ['*'],
     'allowedMethods' => ['*'],
