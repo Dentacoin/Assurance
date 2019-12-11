5c2be7db71a3bed89bf3b8ebe68d73b221197a71
unknown <miroslav.nedelchev@dentacoin.com>
2019-12-11 09:53:05 +0200

    fix facebook btn style

--- resources/views/pages/patient.blade.php
+++ resources/views/pages/patient.blade.php
@@ -188,7 +188,7 @@
                 <div class="col-xs-12 col-sm-5 col-sm-offset-6 col-md-6 col-md-offset-6 text-center">
                     <h3 class="lato-regular fs-30 fs-xs-22 fs-sm-20 white-color padding-bottom-30 padding-bottom-xs-10 padding-bottom-sm-10 text-center">SIGN UP TO GET STARTED</h3>
                     <div class="padding-bottom-10">
-                        <a href="javascript:void(0)" class="facebook-custom-btn social-login-btn calibri-regular fs-20" data-url="//api.dentacoin.com/api/register" data-platform="assurance" data-type="patient">with Facebook</a>
+                        <a href="javascript:void(0)" class="facebook-custom-btn social-login-btn calibri-regular fs-20" data-url="//api.dentacoin.com/api/register" data-platform="assurance" data-type="patient"><i class="fa fa-facebook-official inline-block fs-30 margin-right-20" aria-hidden="true"></i><span class="inline-block">Continue with Facebook</span></a>
                     </div>
                     <div>
                         <a href="javascript:void(0)"  class="civic-custom-btn social-login-btn calibri-regular fs-20" data-url="//api.dentacoin.com/api/register" data-platform="assurance" data-type="patient">with Civic</a>
