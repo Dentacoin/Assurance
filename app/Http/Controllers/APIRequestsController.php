4426995484b8ff23500a2bc1d6595f50c9432961
unknown <miroslav.nedelchev@dentacoin.com>
2020-02-18 14:07:04 +0200

    edit contract accepting logic

--- app/Http/Controllers/APIRequestsController.php
+++ app/Http/Controllers/APIRequestsController.php
@@ -346,8 +346,6 @@ class APIRequestsController extends Controller {
 
         $json = '{"hash":"'.$hash.'", "type":"'.$type.'", "patient_address":"'.$patient_address.'", "dentist_address":"'.$dentist_address.'", "value_usd":"'.$value_usd.'", "monthly_premium_in_dcn":"'.$monthly_premium_in_dcn.'", "time":"'.$time.'", "contract_ipfs_hash":"'.$contract_ipfs_hash.'", "gas_price":"'.$gasPrice.'"}';
 
-        var_dump($json);
-
         curl_setopt_array($curl, array(
             CURLOPT_RETURNTRANSFER => 1,
             CURLOPT_POST => 1,
@@ -360,14 +358,8 @@ class APIRequestsController extends Controller {
                 'Content-Length: ' . mb_strlen($json))
         );
 
-        //$resp = json_decode(curl_exec($curl));
-        $resp = curl_exec($curl);
+        $resp = json_decode(curl_exec($curl));
         curl_close($curl);
-
-        var_dump($resp);
-
-
-        die('asd');
 
         return $resp;
     }
