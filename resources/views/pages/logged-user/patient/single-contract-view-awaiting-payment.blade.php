3a226f29ea64711a0a991d6b99dfe06729263b11
unknown <miroslav.nedelchev@dentacoin.com>
2020-02-19 10:51:53 +0200

    fix admin script

--- resources/views/pages/logged-user/patient/single-contract-view-awaiting-payment.blade.php
+++ resources/views/pages/logged-user/patient/single-contract-view-awaiting-payment.blade.php
@@ -49,7 +49,7 @@
             </div>
             @if(isset($mobile) && $mobile)
                 <div class="row contract-footer show-on-sm">
-                    <div class="col-xs-12 col-sm-8 col-sm-offset-2 padding-top-30 padding-bottom-40 padding-left-50 padding-right-50 text-center fs-20 wrapper padding-top-xs-20 padding-bottom-xs-0 padding-left-xs-0 padding-right-xs-0">
+                    <div class="col-xs-12 col-sm-8 col-sm-offset-2 padding-top-30 text-center fs-20 wrapper padding-top-xs-20 padding-bottom-xs-0 padding-left-0 padding-right-0">
                         <div class="padding-left-15 padding-right-15">
                             <div class="lato-bold fs-20 padding-bottom-5">YOUR FIRST PAYMENT IS DUE IN:</div>
                             <div class="clock"></div>
