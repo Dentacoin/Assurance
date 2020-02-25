50d02e1000365d987ac05052b7d4131c6aab362a
unknown <miroslav.nedelchev@dentacoin.com>
2020-02-25 12:56:47 +0200

    test contract timer dentist view

--- resources/views/pages/logged-user/my-contracts-template.blade.php
+++ resources/views/pages/logged-user/my-contracts-template.blade.php
@@ -6,8 +6,8 @@
     <link rel="shortcut icon" href="{{URL::asset('assets/images/favicon.png') }}" type="image/x-icon" />
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=5" />
     <meta name="csrf-token" content="{{ csrf_token() }}"/>
-    <link rel="stylesheet" type="text/css" href="/dist/css/front-libs-style.css?v=1.0.1">
-    <link rel="stylesheet" type="text/css" href="/assets/css/style.css?v=1.0.1">
+    <link rel="stylesheet" type="text/css" href="/dist/css/front-libs-style.css?v=1.0.2">
+    <link rel="stylesheet" type="text/css" href="/assets/css/style.css?v=1.0.2">
     <script>
         var HOME_URL = '{{ route("home") }}';
     </script>
@@ -18,7 +18,7 @@
             <figure itemscope="" itemtype="http://schema.org/ImageObject" class="inline-block">
                 <img alt="Contracts list" src="/assets/uploads/contracts-list.svg"/>
             </figure>
-            <h2 class="fs-24 lato-bold inline-block">Dentacoin Assurance</h2>
+            <h2 class="fs-24 lato-bold inline-block">My contracts</h2>
         </div>
         @if((new \App\Http\Controllers\UserController())->checkDentistSession())
             <div class="padding-top-20 text-center">
@@ -35,11 +35,11 @@
         @endif
     </div>
     {{--/Show the sticky calculate button only for dentists--}}
-    <script src="/assets/js/basic.js?v=1.0.1"></script>
-    <script src="/dist/js/front-libs-script.js?v=1.0.1"></script>
-    {{--<script src="/dist/js/front-script.js?v=1.0.13"></script>--}}
-    <script src="/assets/js/address.js?v=1.0.1"></script>
-    <script src="/assets/js/index-bundled.js?v=1.0.1"></script>
+    <script src="https://dentacoin.com/assets/js/basic.js?v=1.0.2"></script>
+    <script src="/dist/js/front-libs-script.js?v=1.0.2"></script>
+    {{--<script src="/dist/js/front-script.js?v=1.0.23"></script>--}}
+    <script src="/assets/js/address.js?v=1.0.2"></script>
+    <script src="/assets/js/index-bundled.js?v=1.0.2"></script>
     <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCaVeHq_LOhQndssbmw-aDnlMwUG73yCdk&libraries=places&language=en"></script>
 </body>
 </html>
