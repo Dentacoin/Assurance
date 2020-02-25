95b2387e0700bc51b801b7c52586f39500e05ebb
unknown <miroslav.nedelchev@dentacoin.com>
2020-02-25 11:17:16 +0200

    add contract cancel for awaiting-approval also

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -637,7 +637,7 @@ class UserController extends Controller {
                     $contract->save();
                 }
             } else {
-                var_dump($contract->created_at->toDateString());
+                var_dump($contract->created_at->toDateTimeString());
                 die('asd');
                 // pending
                 if(time() > strtotime($contract->contract_active_at. ' + '.(DAYS_CONTRACT_WITHDRAWAL_PERIOD + GRACE_PERIOD).' days')) {
