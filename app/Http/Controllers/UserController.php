0cf64b1db2a55ae1c71325bc82785b38015feb1a
unknown <miroslav.nedelchev@dentacoin.com>
2020-02-27 08:20:34 +0200

    add automatic page refresh logic when contract status is changd

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -728,7 +728,8 @@ class UserController extends Controller {
         ]);
 
         $contract = TemporallyContract::where(array('patient_id' => session('logged_user')['id'], 'slug' => $request->input('contract')))->orWhere(array('dentist_id' => session('logged_user')['id'], 'slug' => $request->input('contract')))->get()->first();
-        var_dump($contract);
+        var_dump($contract->status);
+        var_dump($request->input('currentStatus'));
         die('asd');
         if(!empty($contract)) {
             var_dump($contract->status);
