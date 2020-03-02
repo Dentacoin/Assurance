e65761628339f624d34c7cbfeeaae596f3d568ef
unknown <miroslav.nedelchev@dentacoin.com>
2020-03-02 10:48:02 +0200

    test record checkup and teeth cleaning

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -806,9 +806,16 @@ class UserController extends Controller {
                         $periodBegin = date('Y-m-d', strtotime(' + ' . (365 * ($yearsActionsToBeExecuted - 1)) . ' days', strtotime($contract->contract_active_at)));
                         $periodEnd = date('Y-m-d', strtotime(' + ' . (365 * $yearsActionsToBeExecuted) . ' days', strtotime($contract->contract_active_at)));
 
-                        $contract['check-ups'] = ContractCheckup::where(array('contract_id' => $contract->id, 'type' => 'check-up', 'approved_by_dentist' => true))->whereBetween('date_at', array($periodBegin, $periodEnd))->get()->all();
+                        var_dump($periodBegin);
+                        var_dump($periodEnd);
 
-                        $contract['teeth-cleanings'] = ContractCheckup::where(array('contract_id' => $contract->id, 'type' => 'teeth-cleaning', 'approved_by_dentist' => true))->whereBetween('date_at', array($periodBegin, $periodEnd))->get()->all();
+                        $contract->check_ups = ContractCheckup::where(array('contract_id' => $contract->id, 'type' => 'check-up', 'approved_by_dentist' => true))->whereBetween('date_at', array($periodBegin, $periodEnd))->get()->all();
+
+                        $contract->teeth_cleanings = ContractCheckup::where(array('contract_id' => $contract->id, 'type' => 'teeth-cleaning', 'approved_by_dentist' => true))->whereBetween('date_at', array($periodBegin, $periodEnd))->get()->all();
+
+                        var_dump($contract->id);
+                        var_dump($contract);
+                        die('asd');
                     }
                 }
 
