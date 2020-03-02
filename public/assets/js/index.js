9d5b760dd23e7a1541d108de690279d9d301fa20
unknown <miroslav.nedelchev@dentacoin.com>
2020-03-02 09:26:41 +0200

    test record checkup and teeth cleaning

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -4745,7 +4745,7 @@ function trackForContractStatusChange(contract, currentStatus) {
 function initDatepicker() {
     if($('.datepicker').length > 0) {
         $('.datepicker').datepicker({
-            format: 'yyyy-mm-dd',
+            dateFormat: 'yyyy-mm-dd',
             startDate: '-3d'
         });
     }
