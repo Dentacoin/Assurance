0ad1ae7ff3148b5959f97a32a362f7dae129046e
unknown <miroslav.nedelchev@dentacoin.com>
2020-03-02 10:24:50 +0200

    test record checkup and teeth cleaning

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -809,6 +809,8 @@ class UserController extends Controller {
                         var_dump($periodBegin);
                         var_dump($periodEnd);
 
+                        die('asd');
+
                         $contract['check-ups'] = ContractCheckup::leftJoin('temporally_contracts', function($join) {
                             $join->on('contract_checkups.contract_id', '=', 'temporally_contracts.id');
                         })->where(array('temporally_contracts.patient_id' => session('logged_user')['id'], 'temporally_contracts.status' => 'active', 'temporally_contracts.slug' => $slug, 'contract_checkups.type' => 'check-up', 'contract_checkups.approved_by_dentist' => true))->whereBetween('contract_checkups.date_at', array($periodBegin, $periodEnd))->get()->all();
