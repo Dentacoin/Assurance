473fad854e23a340e4b62b6bd8c71994970be3ae
unknown <miroslav.nedelchev@dentacoin.com>
2020-03-04 13:26:27 +0200

    test contract cancellation, because of no recorded checkups

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -794,6 +794,14 @@ async function pagesDataOnContractInit() {
                     });
                 }
 
+                if(basic.property_exists(get_params, 'type')) {
+                    if(get_params.type == 'check-up') {
+                        $('.camping-for-popups').addClass('check-up');
+                    } else if(get_params.type == 'teeth-cleaning') {
+                        $('.camping-for-popups').addClass('teeth-cleaning');
+                    }
+                }
+
                 if($('.camping-for-popups').hasClass('check-up') && $('.camping-for-popups').hasClass('teeth-cleaning')) {
                     initCheckUpAndRecordTeethCleaningLogic();
                 } else if($('.camping-for-popups').hasClass('check-up')) {
