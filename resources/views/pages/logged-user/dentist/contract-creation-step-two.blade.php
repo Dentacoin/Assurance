e919af0a34229a62ffdea67c871dd9b0433eabfc
unknown <miroslav.nedelchev@dentacoin.com>
2020-03-11 09:41:59 +0200

    testing new multiple address implementation

--- resources/views/pages/logged-user/dentist/contract-creation-step-two.blade.php
+++ resources/views/pages/logged-user/dentist/contract-creation-step-two.blade.php
@@ -4,7 +4,7 @@
         <div class="single-row flex-row fs-0">
             <label class="calibri-light light-gray-color fs-16 padding-right-15 margin-bottom-0 cursor-pointer" for="patient-email">Email Address:</label>
             @php($patient_email_from_get_param = \Illuminate\Support\Facades\Input::get('patient-email'))
-            <input autocomplete="off" readonly type="text" data-type="email" name="email" id="patient-email" maxlength="100" class="right-field calibri-regular fs-18 dark-color inline-block pencil-background" @if(!empty($patient_email)) value="{{$patient_email}}" @elseif(isset($patient_email_from_get_param)) value="{{$patient_email_from_get_param}}" @endif/>
+            <input autocomplete="off" type="text" data-type="email" name="email" id="patient-email" maxlength="100" class="right-field calibri-regular fs-18 dark-color inline-block pencil-background" @if(!empty($patient_email)) value="{{$patient_email}}" @elseif(isset($patient_email_from_get_param)) value="{{$patient_email_from_get_param}}" @endif/>
         </div>
         <div class="single-row flex-row fs-0">
             <label class="calibri-light light-gray-color fs-16 padding-right-15 margin-bottom-0 cursor-pointer" for="fname">First Name:</label>
