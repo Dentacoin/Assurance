b2852e41a8660a436169b4e674c51731228d350e
unknown <miroslav.nedelchev@dentacoin.com>
2020-03-12 14:27:35 +0200

    reformat js Code

--- public/assets/js/index-bundled.js
+++ public/assets/js/index-bundled.js
@@ -74076,16 +74076,16 @@ var projectData = {
                                     success: async function (response) {
                                         console.log(response, 'response');
                                         if(response.success) {
+                                            console.log(response.data.length, 'response.data.length');
                                             for (var i = 0, len = response.data.length; i < len; i+=1) {
-                                                if(foundContracts.indexOf(response.data[i].slug) != -1) {
+                                                console.log(response.data[i], 'response.data.length');
+                                                if(foundContracts.indexOf(response.data[i].slug) == -1) {
                                                     foundContracts.push(response.data[i].slug);
 
                                                     $('.contracts-list').slick('slickAdd','<a href="https://assurance.dentacoin.com/patient/contract/'+response.data[i].slug+'" class="module contract-tile padding-bottom-10 pending"><div class="tile-wrapper fs-0"> <div class="inline-block-top figure-container"><figure itemscope="" itemtype="http://schema.org/ImageObject"><img alt="Dentist avatar" src="'+response.data[i].dentistAvatar+'"><figcaption class="fs-14 calibri-light text-center padding-left-5 padding-right-5">'+response.data[i].status+'</figcaption></figure></div><div class="contract-info inline-block-top"><div class="calibri-bold fs-18 title">'+response.data[i].dentistName+'</div><time class="display-block fs-14 calibri-light">Sent on: '+response.data[i].createdAt+'</time><div class="lato-semibold fs-24 line-height-24">'+response.data[i].monthlyPremium+'$</div><div class="btn-container"><div class="white-blue-green-btn">'+response.data[i].btnLabel+'</div></div></div></div></a>', 0);
                                                 }
                                             }
                                         }
-
-                                        //$('.contracts-list').slick('slickAdd','<a href="https://assurance.dentacoin.com/patient/contract/0siOzDNv9ilFpLSOZUtd6Dq7xTeQytxmeFLna9GW8IRY2XM7DpnhV4eb5WpP0h1582114787" class="module test123 contract-tile padding-bottom-10 pending"> <div class="tile-wrapper fs-0" style="height: 151.6px;"> <div class="inline-block-top figure-container"> <figure itemscope="" itemtype="http://schema.org/ImageObject"> <img alt="Dentist avatar" src="https://reviews.dentacoin.com/storage/avatars/34/70134.jpg?rev=1583998339"> <figcaption class="fs-14 calibri-light text-center padding-left-5 padding-right-5"> Cancelled </figcaption> </figure> </div> <div class="contract-info inline-block-top"> <div class="calibri-bold fs-18 title">333. Miroslav Nedelchev</div> <time class="display-block fs-14 calibri-light">Sent on: 19/02/2020</time> <div class="lato-semibold fs-24 line-height-24">20$</div> <div class="btn-container"> <div class="white-blue-green-btn">Details</div> </div> </div> </div> </a>', 0);
                                     }
                                 });
                             }, 3000);
