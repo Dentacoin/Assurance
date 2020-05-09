ff1300e9d936e37dc21cb88da27cbf0582abf6c1
unknown <miroslav.nedelchev@dentacoin.com>
2020-05-09 18:26:37 +0300

    test animation

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1585,8 +1585,8 @@ var projectData = {
                                 //not enough DCN
                                 $('.camping-for-popups').append('<div class="col-xs-12 col-sm-8 col-sm-offset-2 col-lg-6 col-lg-offset-3 text-center fs-20 contract-response-message module"><div class="wrapper text-center padding-top-30"><figure itemscope="" itemtype="http://schema.org/ImageObject"><img alt="Fund icon" src="/assets/uploads/fund-icon.svg" class="max-width-70"/></figure><h2 class="lato-bold fs-22 padding-top-15 blue-green-color">YOUR CONTRACT</h2><h3 class="fs-22 padding-top-5 lato-bold">Time to fund your account now!</h3><div class="fs-18 fs-xs-16 calibri-light padding-top-15 padding-bottom-25">You should fund your account with DCN equivalent to <span class="calibri-bold blue-green-color">'+$('.patient-contract-single-page-section').attr('data-monthly-premium')+' USD</span> (at the moment: <span class="calibri-bold blue-green-color">'+monthly_premium_in_dcn+' DCN</span>) before <span class="calibri-bold blue-green-color">'+projectData.utils.dateObjToFormattedDate(next_payment_timestamp_date_obj)+'</span>.</div><div><a href="javascript:void(0)" class="white-blue-green-btn min-width-150 scroll-to-buy-section">FUND NOW</a></div></div></div>');
 
-                                if (patientEthDcnBalanceLogicAnimation) {
-                                    patientEthDcnBalanceLogicAnimation = false;
+                                if (patientDcnBalanceLogicAnimation) {
+                                    patientDcnBalanceLogicAnimation = false;
                                     $('.camping-for-popups .wrapper').addClass('box-shadow-animation');
                                 }
 
