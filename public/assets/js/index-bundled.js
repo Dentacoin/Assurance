bc60431aff7fa9d1111455cf719b96d5613e2f7e
unknown <miroslav.nedelchev@dentacoin.com>
2020-06-10 16:09:27 +0300

    fix pdf field with long text

--- public/assets/js/index-bundled.js
+++ public/assets/js/index-bundled.js
@@ -73917,7 +73917,6 @@ var projectData = {
             }
         },
         clearUrlFromGetParam(regex) {
-            console.log(regex, 'clearUrlFromGetParam');
             // remove get param from url
             history.replaceState && history.replaceState(
                 null, '', location.pathname + location.search.replace(regex, '').replace(/^&/, '?')
@@ -75804,7 +75803,51 @@ var projectData = {
                                                                 withdraw_transaction.sign(new Buffer(transaction_key, 'hex'));
 
                                                                 //sending the transaction
-                                                                dApp.web3_1_0.eth.sendSignedTransaction('0x' + withdraw_transaction.serialize().toString('hex'), function (err, transactionHash) {
+                                                                dApp.web3_1_0.eth.sendSignedTransaction('0x' + withdraw_transaction.serialize().toString('hex')).on('transactionHash', function(transactionHash) {
+                                                                    console.log(transactionHash, 'transactionHash');
+                                                                    var execute_ajax = true;
+                                                                    //doing setinterval check to check if the smart creation transaction got mined
+                                                                    var withdraw_interval_check = setInterval(async function() {
+                                                                        var withdraw_status = await dApp.web3_1_0.eth.getTransactionReceipt(transactionHash);
+                                                                        console.log(withdraw_status, 'withdraw_status');
+                                                                        if (withdraw_status != null && basic.property_exists(withdraw_status, 'status')) {
+                                                                            if (withdraw_status.status && execute_ajax) {
+                                                                                execute_ajax = false;
+                                                                                clearInterval(withdraw_interval_check);
+                                                                                console.log('SENDING AJAX');
+
+                                                                                //SEND EMAIL TO PATIENT
+                                                                                $.ajax({
+                                                                                    type: 'POST',
+                                                                                    url: '/dentist/notify-patient-for-successful-withdraw',
+                                                                                    dataType: 'json',
+                                                                                    data: {
+                                                                                        transaction_hash: transactionHash,
+                                                                                        contract: $('.single-contract-view-section').attr('data-contract')
+                                                                                    },
+                                                                                    headers: {
+                                                                                        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                                                                                    },
+                                                                                    success: async function (response) {
+                                                                                        history.pushState({},'', '?successful-withdraw=' + transactionHash);
+                                                                                        // window.location.reload();
+                                                                                    }
+                                                                                });
+                                                                            }
+                                                                        }
+                                                                    }, 3000);
+                                                                }).on('confirmation', function(confirmationNumber, receipt) {
+                                                                    console.log(confirmationNumber, 'confirmationNumber');
+                                                                    console.log(receipt, 'receipt');
+                                                                    console.log(' ========= CONFIRMATION ===========');
+                                                                }).on('error', function(error) {
+                                                                    console.log(error, 'error');
+                                                                    basic.showAlert('Something went wrong, please try again later.', 'boobox-alert', true);
+                                                                    hideLoader();
+                                                                });
+
+
+                                                                /*dApp.web3_1_0.eth.sendSignedTransaction('0x' + withdraw_transaction.serialize().toString('hex'), function (err, transactionHash) {
                                                                     console.log(transactionHash, 'transactionHash');
                                                                     var execute_ajax = true;
                                                                     //doing setinterval check to check if the smart creation transaction got mined
@@ -75834,16 +75877,10 @@ var projectData = {
                                                                                         window.location.reload();
                                                                                     }
                                                                                 });
-                                                                                /*hideLoader();
-
-                                                                                basic.showDialog('<div class="text-center padding-top-30"><svg class="max-width-50" version="1.1" id="Layer_1" xmlns:x="&ns_extend;" xmlns:i="&ns_ai;" xmlns:graph="&ns_graphs;"xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 64 82"style="enable-background:new 0 0 64 82;" xml:space="preserve"><style type="text/css">.st0{fill:#126585;}  .st1{fill-rule:evenodd;clip-rule:evenodd;fill:#126585;}</style><metadata><sfw  xmlns="&ns_sfw;"><slices></slices><sliceSourceBounds  bottomLeftOrigin="true" height="82" width="64" x="18" y="34"></sliceSourceBounds></sfw></metadata><g transform="translate(0,-952.36218)"><g><path class="st0" d="M31.7,952.4c-0.1,0-0.3,0.1-0.4,0.1l-30,11c-0.8,0.3-1.3,1-1.3,1.9v33c0,7.8,4.4,14.3,10.3,20c5.9,5.7,13.5,10.7,20.5,15.7c0.7,0.5,1.6,0.5,2.3,0c7-5,14.6-10,20.5-15.7c5.9-5.7,10.3-12.2,10.3-20v-33c0-0.8-0.5-1.6-1.3-1.9l-30-11C32.4,952.4,32,952.3,31.7,952.4z M32,956.5l28,10.3v31.6c0,6.3-3.5,11.8-9.1,17.1c-5.2,5-12.2,9.7-18.9,14.4c-6.7-4.7-13.7-9.4-18.9-14.4c-5.5-5.3-9.1-10.8-9.1-17.1v-31.6L32,956.5z"/></g></g><g><g><path class="st1" d="M50.3,25.9c0.6,0.6,1.2,1.2,1.8,1.8c0.9,0.9,0.9,2.5,0,3.4C45.6,37.5,39.1,44,32.6,50.5c-3.3,3.3-3.5,3.3-6.8,0c-3.3-3.3-6.7-6.7-10-10c-0.9-0.9-0.9-2.5,0-3.4c0.6-0.6,1.2-1.2,1.8-1.8c0.9-0.9,2.5-0.9,3.4,0c2.7,2.7,5.4,5.4,8.2,8.2c5.9-5.9,11.7-11.7,17.6-17.6C47.8,25,49.3,25,50.3,25.9z"/></g></g></svg><div class="lato-bold fs-30">SUCCESSFULLY WITHDRAWN</div><div class="padding-top-20 padding-bottom-15 fs-20">You have successfully withdrawn your Dentacoins from this contract. You will be notified via email when next withdraw is possible.</div><div class="btn-container padding-bottom-40"><a href="javascript:void(0)" class="white-blue-green-btn min-width-200 close-popup">OK</a></div></div>', '', null, true);
-                                                                                setTimeout(function() {
-                                                                                    window.location.reload();
-                                                                                }, 3000);*/
                                                                             }
                                                                         }
                                                                     }, 3000);
-                                                                });
+                                                                });*/
                                                             }, 2000);
                                                         }
                                                     }
