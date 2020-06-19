78cfcff756bdf531481b6ed72e447842809d08ea
unknown <miroslav.nedelchev@dentacoin.com>
2020-06-19 10:13:57 +0300

    test js

--- public/assets/js/index-bundled.js
+++ public/assets/js/index-bundled.js
@@ -78746,8 +78746,8 @@ function markContractAsProcessing(slug) {
         },
         success: function(response) {
             if(response.success) {
-                var mobileHtml = '<div class="row"><div class="attention-in-process fs-18 text-center padding-bottom-15 col-xs-12"><a href="javascript:void(0);">ATTENTION: ACTION IN PROCESS <img src="/assets/images/question-mark.svg" class="margin-left-5 width-100 max-width-20" alt="Question mark"/></a></div></div>';
-                var desktopHtml = '<div class="attention-in-process fs-16 text-center padding-bottom-10"><a href="javascript:void(0);">ATTENTION: ACTION IN PROCESS <img src="/assets/images/question-mark.svg" class="margin-left-5 width-100 max-width-20" alt="Question mark"/></a></div>';
+                var mobileHtml = '<div class="row"> <div class="attention-in-process fs-18 text-center padding-bottom-15 col-xs-12"> <a href="javascript:void(0);"> <div class="padding-bottom-10"><img src="/assets/images/processing-contract-loading.gif" class="width-100 max-width-80" alt="Processing contact loader"/></div>ATTENTION: ACTION IN PROCESS <img src="/assets/images/question-mark.svg" class="margin-left-5 width-100 max-width-20" alt="Question mark"/> </a> </div></div>';
+                var desktopHtml = '<div class="attention-in-process fs-16 text-center padding-bottom-10 padding-left-10 padding-right-10"> <a href="javascript:void(0);"> <div class="padding-bottom-10"><img src="/assets/images/processing-contract-loading.gif" class="width-100 max-width-80" alt="Processing contact loader"/></div>ATTENTION: ACTION IN PROCESS <img src="/assets/images/question-mark.svg" class="margin-left-5 width-100 max-width-20" alt="Question mark"/> </a></div>';
 
                 if ($('body').hasClass('logged-patient')) {
                     $('.patient-contract-single-page-section').attr('data-processing-contract', 'true');
@@ -78776,6 +78776,8 @@ function markContractAsProcessing(slug) {
                     }
                     $('.contract-body .steps-navigation').remove();
                 }
+
+                $('html, body').animate({scrollTop: 0}, 300);
             }
         }
     });
