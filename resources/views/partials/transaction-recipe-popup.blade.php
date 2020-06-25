f4e093485dc435e5e740652db216572cd27a155e
unknown <miroslav.nedelchev@dentacoin.com>
2020-06-25 10:30:49 +0300

    test dentist withdraw

--- resources/views/partials/transaction-recipe-popup.blade.php
+++ resources/views/partials/transaction-recipe-popup.blade.php
@@ -8,6 +8,7 @@
 @elseif (!empty($withdrawableDCN))
     @php($dentacoin_data = (new \App\Http\Controllers\APIRequestsController())->getDentacoinDataByExternalProvider())
     <div class="section-blue-green-background text-center">
+        {{var_dump($dentacoin_data['USD'] * $withdrawableDCN)}}
         <div class="usd_val">$<span>{{number_format($dentacoin_data['USD'] * $withdrawableDCN, 2, '.', '')}}</span></div>
         <div class="dcn_val">= <span>{{$withdrawableDCN}}</span> DCN</div>
     </div>
