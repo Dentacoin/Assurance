78aaf9e67fa357081beee64906048e108724ef22
unknown <miroslav.nedelchev@dentacoin.com>
2020-06-25 14:19:59 +0300

    change version of resources

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -25,12 +25,12 @@
     <style>
 
     </style>
-    <link rel="stylesheet" type="text/css" href="/dist/css/front-libs-style.css?v=1.0.63">
-    <link rel="stylesheet" type="text/css" href="/assets/css/style.css?v=1.0.63">
+    <link rel="stylesheet" type="text/css" href="/dist/css/front-libs-style.css?v=1.0.64">
+    <link rel="stylesheet" type="text/css" href="/assets/css/style.css?v=1.0.64">
     @if((new \App\Http\Controllers\UserController())->checkSession())
-        <link rel="stylesheet" type="text/css" href="https://dentacoin.com/assets/libs/dentacoin-mini-hub/css/style.css?v=1.0.63">
+        <link rel="stylesheet" type="text/css" href="https://dentacoin.com/assets/libs/dentacoin-mini-hub/css/style.css?v=1.0.64">
     @else
-        <link rel="stylesheet" type="text/css" href="https://dentacoin.com/assets/libs/dentacoin-login-gateway/css/dentacoin-login-gateway-style.css?v=1.0.63"/>
+        <link rel="stylesheet" type="text/css" href="https://dentacoin.com/assets/libs/dentacoin-login-gateway/css/dentacoin-login-gateway-style.css?v=1.0.64"/>
 @endif
 
     <!-- Global site tag (gtag.js) - Google Analytics -->
@@ -293,17 +293,17 @@
     </div>
 
     {{--/Show the sticky calculate button only for dentists--}}
-    <script src="https://dentacoin.com/assets/js/basic.js?v=1.0.63"></script>
-    <script src="/dist/js/front-libs-script.js?v=1.0.63"></script>
+    <script src="https://dentacoin.com/assets/js/basic.js?v=1.0.64"></script>
+    <script src="/dist/js/front-libs-script.js?v=1.0.64"></script>
     {{--<script src="/dist/js/front-script.js?v=1.0.13"></script>--}}
     @if((new \App\Http\Controllers\UserController())->checkSession())
-        <script src="https://dentacoin.com/assets/libs/dentacoin-mini-hub/js/init.js?v=1.0.63"></script>
+        <script src="https://dentacoin.com/assets/libs/dentacoin-mini-hub/js/init.js?v=1.0.64"></script>
     @else
-        <script src="https://dentacoin.com/assets/libs/dentacoin-login-gateway/js/init.js?v=1.0.63"></script>
+        <script src="https://dentacoin.com/assets/libs/dentacoin-login-gateway/js/init.js?v=1.0.64"></script>
     @endif
-    <script src="https://dentacoin.com/assets/js/address-combined-login.js?v=1.0.63"></script>
+    <script src="https://dentacoin.com/assets/js/address-combined-login.js?v=1.0.64"></script>
 
-    <script src="/assets/js/index-bundled.js?v=1.0.63"></script>
+    <script src="/assets/js/index-bundled.js?v=1.0.64"></script>
     <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCaVeHq_LOhQndssbmw-aDnlMwUG73yCdk&libraries=places&language=en"></script>
 
     {{--Multiple errors from laravel validation--}}
