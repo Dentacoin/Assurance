fd50d6ed717d06438b8b59421285c90b543a2ac9
unknown <miroslav.nedelchev@dentacoin.com>
2020-07-02 12:48:29 +0300

    add header

--- app/Http/Middleware/AdditionalMiddleware.php
+++ app/Http/Middleware/AdditionalMiddleware.php
@@ -24,6 +24,7 @@ class AdditionalMiddleware
         $response = $next($request);
         $response->headers->set('Referrer-Policy', 'no-referrer');
         $response->headers->set('X-XSS-Protection', '1; mode=block');
+        $response->headers->set('Access-Control-Allow-Headers', 'Origin');
         return (new App\Http\Controllers\Controller())->minifyHtml($response);
     }
 }
