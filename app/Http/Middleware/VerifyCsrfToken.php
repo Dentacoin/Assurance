69082d7b12ee65f81179bdb7f79c066d32d48571
unknown <miroslav.nedelchev@dentacoin.com>
2021-06-10 07:47:25 +0300

    fix login

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -14,7 +14,7 @@ class VerifyCsrfToken extends BaseVerifier
      * @var array
      */
     protected $except = [
-        'save-public-key', 'cancel-contracts', 'get-scanning-data', 'request-contract-status-change', 'info/*', 'mark-contract-as-processing'
+        'save-public-key', 'cancel-contracts', 'get-scanning-data', 'request-contract-status-change', 'info/*', 'mark-contract-as-processing', 'authenticate-user'
     ];
 
     protected function addCookieToResponse($request, $response) {
